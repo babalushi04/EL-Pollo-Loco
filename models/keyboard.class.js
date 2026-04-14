e9ae39e14b170edca9aed95ee66b597eab701f2d
babalushi04 <xhini95@gmail.com>
2026-04-15 00:52:18 +0200

    Refactor Keyboard class to represent keyboard input state with key tracking

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,19 +1,11 @@
 /**
- * Represents a collectable health heart that restores the character's energy.
- * @extends MoveableObject
+ * Represents the keyboard input state, tracking which keys are currently pressed.
  */
-class HealthHeart extends MoveableObject {
-  height = 50;
-  width = 50;
-
-  /**
-   * Creates a new HealthHeart at the given position.
-   * @param {number} x - The x-coordinate.
-   * @param {number} y - The y-coordinate.
-   */
-  constructor(x, y) {
-    super().loadImage("img/7_statusbars/3_icons/icon_health.png");
-    this.x = x;
-    this.y = y;
-  }
+class Keyboard {
+  LEFT = false;
+  RIGHT = false;
+  UP = false;
+  DOWN = false;
+  SPACE = false;
+  D = false;
 }
